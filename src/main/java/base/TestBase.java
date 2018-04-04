a4de6fd2b0518bce9b3803bc59bdc1252e638ebd
Borson Khan <borsonkh@gmail.com>
2018-04-04 14:54:11 -0400

    data input

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -1,5 +1,38 @@
 package base;
 
-public class TestBase {
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class TestBase extends Saucelabs{
+
+	@BeforeSuite
+	public void setUp() {
+		
+		
+		if(Boolean.valueOf(getProperty("saucelabs"))){
+			
+			setSaucelabs();
+		}else {
+			
+		}
+		setDriver();
+		
+		
+	}
+	
+	@Test
+	
+	public void test() {
+		
+	
+	}
+	
+	@AfterSuite
+	
+	public void tearDown() {
+		driver.quit();
+		
+	}
 }
--- src/main/java/utils/ReadProperties.java
+++ src/main/java/utils/ReadProperties.java
@@ -6,15 +6,41 @@ import java.util.Properties;
 
 public class ReadProperties {
 
-	public static void main(String args[]) {
-		System.out.println(loadPropertes("QA"));
+	public String getProperty(String key) {
+		String value = null;
 
+		key = key.trim();
+		Properties properFileName = loadPropertes("testRun");
+
+		value = properFileName.getProperty(key);
+
+		if (value == null) {
+			String testEnv = properFileName.getProperty("testEnv");
+
+			properFileName = loadPropertes(testEnv);
+
+			value = properFileName.getProperty(key);
+
+		}
+
+		return value;
 	}
 
-	public static Properties loadPropertes(String properFileName) {
+	public String getSaucelabsProperty(String key) {
+		String value = null;
+
+		key = key.trim();
+		Properties properFileName = loadPropertes("saucelabs");
+
+		value = properFileName.getProperty(key);
+
+		return value;
+	}
+
+	public Properties loadPropertes(String properFileName) {
 		Properties properFile = new Properties();
 
-		String filePath = ".\\src\\main\\java\\resources\\properties\\" + properFileName.trim() + ".properties";
+		String filePath = ".\\src\\main\\resources\\properties\\" + properFileName.trim() + ".properties";
 
 		try {
 			File file = new File(filePath);
