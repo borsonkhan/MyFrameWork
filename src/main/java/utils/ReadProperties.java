832a9667b910ce1d613ed49e11c592331a544f50
Borson Khan <borsonkh@gmail.com>
2018-04-04 03:05:47 -0400

    king

--- src/main/java/utils/ReadProperties.java
+++ src/main/java/utils/ReadProperties.java
@@ -1,5 +1,29 @@
 package utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class ReadProperties {
 
+	public static void main(String args[]) {
+		System.out.println(loadPropertes("QA"));
+
+	}
+
+	public static Properties loadPropertes(String properFileName) {
+		Properties properFile = new Properties();
+
+		String filePath = ".\\src\\main\\java\\resources\\properties\\" + properFileName.trim() + ".properties";
+
+		try {
+			File file = new File(filePath);
+			FileInputStream fileStream = new FileInputStream(file);
+			properFile.load(fileStream);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return properFile;
+	}
 }
